64596476cbd3f3be11f69837fad784b99fc5c130
Sebastian Haag <s.haag@siqens.local>
2021-02-12 19:07:09 +0100

    Finally got the restructuring of the FileSystemAPI response to the desired array/objects structure for vue-jstree working

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,14 +13,15 @@
 </template>
 
 <script>
-  let fileHandle; //Declaring a varibale here makes it available for all methods below
+  //Declaring a varibale here makes it available for all methods below
+  let fileHandle;
   let folder;
   let treeData = [];
   let i = 0;
   let id;
   let lastKeyIndex;
   let key;
-  let index;
+  let parentIndex; //The index of the array of the parent object
   let arrayLength;
   let newArrayLength;
 
@@ -50,18 +51,21 @@
       },
 
       async openFolder() { //Source: https://www.youtube.com/watch?v=csCk4mrEmm8
+        treeData = [];
         folder = await window.showDirectoryPicker();
         await this.getFiles(folder);
         console.log(treeData)
       },
 
-      async getFiles(folder, parent = null, parentIndex = 0, array = []) {
+      async getFiles(folder, parentId = null, path = []) {
         for await (const entry of folder.values()) {
+          
+          //i has to be incrimented at the beginning because this for function is nested and multiple times called within itself befor finishing
           i++
           id = i-1
 
           if (entry.kind === "file") {
-            if (parent == null) {
+            if (parentId == null) { //parent == null is the initial state when the function is first run
               treeData.push ({ //Array/Object structure: https://zdy1988.github.io/vue-jstree/
                 id: id,
                 text: entry.name,
@@ -71,12 +75,12 @@
                 selected: "false",
                 disabled: "false",
                 loading: "false",
-                parent: parent ?? '#',
+                parent: parentId ?? '#',
                 children: []
               })
             }
             else {
-              await this.assign(treeData, array, {
+              await this.assign(treeData, path, {
                 id: id,
                 text: entry.name,
                 value: "",
@@ -85,13 +89,17 @@
                 selected: "false",
                 disabled: "false",
                 loading: "false",
-                parent: parent ?? '#',
+                parent: parentId ?? '#',
                 children: []
               })
             }
+            // console.log('file_id: ' + id)
+            // console.log('file_path: ' + path)
+            // console.log('parentId: ' + parentId)
+            // console.log('_____________________________________________')
           }
           else if (entry.kind === "directory") {
-            if (parent == null) {
+            if (parentId == null) {
               treeData.push ({
                 id: id,
                 text: entry.name,
@@ -101,12 +109,12 @@
                 selected: "false",
                 disabled: "false",
                 loading: "false",
-                parent: parent ?? '#',
+                parent: parentId ?? '#',
                 children: []
               })
             }
             else {
-              await this.assign(treeData, array, {
+              await this.assign(treeData, path, {
                 id: id,
                 text: entry.name,
                 value: "",
@@ -115,33 +123,52 @@
                 selected: "false",
                 disabled: "false",
                 loading: "false",
-                parent: parent ?? '#',
+                parent: parentId ?? '#',
                 children: []
               })
             }
             //Going deeper in the folder structure by opening another directory by accessing this function again (nested)
             //Therefore updating the array to pass the current tree path to the assign function later
-            index = treeData.findIndex(x => x.id === parentIndex) //Source: https://stackoverflow.com/questions/15997879/get-the-index-of-the-object-inside-an-array-matching-a-condition
             
-            // await this.getIndex(treeData, array)
+            //This is hardcoded. The index has to be calculated from the parentId. How?
+            // if (id == 3) {
+            //   parentIndex = 1
+            // }
+            // else {
+            //   parentIndex = 0
+            // }
 
-            array.push(parentIndex);
-            array.push('children');
+            if (parentId == null) {
+              parentIndex = 0
+            }
+            else {
+              await this.getIndex(treeData, path, id)
+            }
             
-            await this.getFiles(entry, id, index, array)
+            // console.log('dir_id: ' + id)
+            // console.log('dir_path: ' + path)
+            // console.log('parentId: ' + parentId)
+            // console.log('parentIndex: ' + parentIndex)
+
+            // Creating the path of this directory
+            path.push(parentIndex); //parentIndex = 0 is the initial state when the function is first run (hence at this part the first directory is opened)
+            path.push('children');
+
+            await this.getFiles(entry, id, path)
+
           }
         }
         //When reaching this part of the function the deeper laying folder structures have been finished processing
         //Therefore updating the array to move up one folder in the path
-        arrayLength = array.length
+        arrayLength = path.length
         if (arrayLength >> 0) {
           newArrayLength = arrayLength - 2
         }
-        array.length = newArrayLength
+        path.length = newArrayLength
       },
 
       async assign(obj, keyPath, value) { //Source: https://stackoverflow.com/questions/5484673/javascript-how-to-dynamically-create-nested-objects-using-object-names-given-by
-        lastKeyIndex = keyPath.length-1;
+        lastKeyIndex = keyPath.length - 1;
         for (var i = 0; i < lastKeyIndex; ++ i) {
           key = keyPath[i];
           if (!(key in obj)){
@@ -152,8 +179,8 @@
         obj[keyPath[lastKeyIndex]].push(value);
       },
 
-      async getIndex(obj, keyPath) {
-        lastKeyIndex = keyPath.length-1;
+      async getIndex(obj, keyPath, id) {
+        lastKeyIndex = keyPath.length - 1;
         for (var i = 0; i < lastKeyIndex; ++ i) {
           key = keyPath[i];
           if (!(key in obj)){
@@ -161,10 +188,18 @@
           }
           obj = obj[key];
         }
-        index = obj[keyPath[lastKeyIndex]].findIndex(x => x.id === 2)
-        console.log(obj[keyPath[lastKeyIndex]])
-        console.log(index)
+        parentIndex = obj[keyPath[lastKeyIndex]].findIndex(x => x.id === id)
+
+        // console.log('_____________________________________________')
+        // console.log('path: ' + keyPath)
+        // console.log('id: ' + id)
+        // console.log(obj[keyPath[lastKeyIndex]])
+        // console.log(obj[keyPath[lastKeyIndex]].findIndex(x => x.id === id))
+        // console.log(parentIndex)
+        // console.log('.............................................')
       },
+
+
 
       keyDown: function () {
         const activeElement = document.getElementsByClassName('active')[0]
